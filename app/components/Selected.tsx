63deb04ea0b9b1ad42e6685c2f62f721485b0bc5
Daniel Lehre Ekeberg <danieleke@live.no>
2025-12-15 23:15:16 +0100

    Changed from Holidaze to DanBook

--- app/components/Selected.tsx
+++ app/components/Selected.tsx
@@ -11,7 +11,7 @@ export default function SelectedWork() {
                 </div>
                 <p className="text-neutral-400 text-xl">A curated selection of work that showcase my passion for creating exceptional digital experiences.</p>
             </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 mt-15">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-10 md:gap-0 mt-15">
                 <Link href="../project/luxelot" className="rounded-xl overflow-hidden border group bg-neutral-900 border-neutral-600/20 hover:-translate-y-2 transition duration-300 cursor-pointer">
                     <img className="group-hover:grayscale-0 grayscale transition duration-300" src="../luxelot-teaser.jpg" />
                     <div className="p-5">
@@ -23,7 +23,7 @@ export default function SelectedWork() {
                         <p className="text-neutral-400 my-5">LuxeLot is a front-end auction marketplace featuring user authentication, live bidding, and listing management, designed to deliver a smooth and intuitive user experience.</p>
                     </div>
                 </Link>
-                <div className="p-10">
+                <div className="md:p-10">
                     <Link href="../project/corely" className="block rounded-xl overflow-hidden border border-neutral-600/20 group bg-neutral-900 hover:-translate-y-2 transition duration-300 cursor-pointer">
                         <img className="group-hover:grayscale-0 grayscale transition duration-300" src="../corely-teaser.jpg" />
                         <div className="p-5">
@@ -37,16 +37,16 @@ export default function SelectedWork() {
                     </Link>
                 </div>
             </div>
-            <div className="flex justify-center">
-                <Link href="../project/holidaze" className="rounded-xl w-250 overflow-hidden border border-neutral-600/20 group bg-neutral-900 hover:-translate-y-2 transition duration-300 cursor-pointer">
-                    <img className="group-hover:grayscale-0 grayscale transition duration-300" src="../holidaze-teaser.jpg" />
+            <div className="flex justify-center mt-10">
+                <Link href="../project/danbook" className="rounded-xl w-250 overflow-hidden border border-neutral-600/20 group bg-neutral-900 hover:-translate-y-2 transition duration-300 cursor-pointer">
+                    <img className="group-hover:grayscale-0 grayscale transition duration-300" src="../danbook-teaser.png" />
                     <div className="p-5">
                         <div className="flex items-center gap-2">
                             <div className="h-[1px] bg-orange-500 w-5" />
                             <p className="text-orange-500 text-sm my-2">PROJECT 03</p>
                         </div>
-                        <h1 className="text-3xl font-extrabold group-hover:text-orange-500 transition duration-300">Holidaze</h1>
-                        <p className="text-neutral-400 my-5">Holidaze is a modern booking platform that lets users explore, search, and discover unique places to stay around the world, with a strong focus on usability and clean design.</p>
+                        <h1 className="text-3xl font-extrabold group-hover:text-orange-500 transition duration-300">DanBook</h1>
+                        <p className="text-neutral-400 my-5">DanBook is a social blogging platform where users can create posts, interact with others, and manage their personal profiles in a modern, dark-themed interface.</p>
                     </div>
                 </Link>
             </div>
