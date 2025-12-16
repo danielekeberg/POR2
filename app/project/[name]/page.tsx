a4727aa63b55b85483df4239c5bbcf8a3c7c3f3c
Daniel Lehre Ekeberg <danieleke@live.no>
2025-12-16 01:05:30 +0100

    Made Project page mobile friendly

--- app/project/[name]/page.tsx
+++ app/project/[name]/page.tsx
@@ -31,13 +31,13 @@ export default function Project() {
     return(
         <>
             <Header />
-            <div className="px-5 md:px-[15%] py-35">
-                <Link href="../" className="flex gap-2 items-center text-neutral-400 group cursor-pointer">
+            <div className="px-5 md:px-[15%] py-25 md:py-35">
+                <Link href="../projects/" className="flex gap-2 items-center text-neutral-400 group cursor-pointer">
                     <img src="../arrow-left.svg" className="h-5 group-hover:-translate-x-1 transition duration-300" />
                     <p className="group-hover:text-[#eae8e0]">Back to projects</p>
                 </Link>
                 <p className="text-orange-500 mt-10 mb-5">{project?.category}</p>
-                <h1 className="text-8xl font-bold my-5">{project?.name}</h1>
+                <h1 className="text-5xl md:text-8xl font-bold my-5">{project?.name}</h1>
                 <p className="text-neutral-400 my-10">{project?.desc}</p>
                 <div className="grid grid-cols-1 md:flex gap-5 items-center text-sm">
                     <div className="flex flex-col gap-3">
@@ -48,7 +48,7 @@ export default function Project() {
                         <p className="text-neutral-400">TECHNOLOGIES</p>
                         <div className="grid grid-cols-2 md:grid-cols-4 gap-2 items-center">
                             {project?.tech.map((t:any, i:any) => (
-                                <p key={i} className="bg-neutral-900 text-center py-1 px-3 rounded-full border border-neutral-400/10">{t}</p>
+                                <p key={i} className="bg-neutral-900 text-center py-1 px-3 rounded-full border border-neutral-400/10 hover:border-orange-500 cursor-pointer transition duration-300">{t}</p>
                             ))}
                         </div>
                     </div>
@@ -60,14 +60,14 @@ export default function Project() {
                     <h1 className="text-3xl font-bold mb-10">Project Overview</h1>
                     <p>{project?.description}</p>
                 </div>
-                <div className="my-15">
+                <div className="mt-15 md:my-15">
                     <h1 className="text-center text-4xl font-bold">Want to see it live?</h1>
-                    <div className="flex gap-10 justify-center my-3">
-                    <Link target="_blank" href={`${project?.live}`} className="flex group items-center gap-2 bg-orange-500 px-8 py-5 text-neutral-900 font-bold rounded-full hover:scale-110 cursor-pointer transition hover:shadow-lg shadow-orange-500/15">
+                    <div className="grid grid-cols-1 md:grid-cols-2 text-center md:flex gap-5 md:gap-10 justify-between md:justify-center my-5">
+                    <Link target="_blank" href={`${project?.live}`} className="flex group justify-center items-center border border-orange-500 gap-2 bg-orange-500 px-2 md:px-8 py-5 text-neutral-900 font-bold rounded-full hover:scale-110 cursor-pointer transition hover:shadow-lg shadow-orange-500/15">
                         <p>View Live Project</p>
                         <img className="h-5 group-hover:-translate-y-1 transition duration-300" src="../external.svg" />
                     </Link>
-                    <Link target="_blank" href={`${project?.github}`} className="bg-neutral-900 px-8 py-5 text-[#eae8e0] font-bold rounded-full hover:scale-110 cursor-pointer transition hover:shadow-lg shadow-neutral-500/15">Github</Link>
+                    <Link target="_blank" href={`${project?.github}`} className="bg-neutral-900 border border-neutral-400/20 px-2 md:px-8 py-5 text-[#eae8e0] font-bold rounded-full hover:scale-110 cursor-pointer transition hover:shadow-lg shadow-neutral-500/15">Github</Link>
                 </div>
                 </div>
             </div>
