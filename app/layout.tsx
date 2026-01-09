6e4420f7208800536c22971a90f4eaba77d5a30b
Daniel Lehre Ekeberg <danieleke@live.no>
2026-01-09 23:12:04 +0100

    Add Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,28 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import { DM_Sans } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import { DM_Sans } from 'next/font/google';
+import './globals.css';
+import { Analytics } from '@vercel/analytics/next';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const dmSans = DM_Sans({
-  subsets: ["latin"],
-  weight: ["400", "500", "700", "1000"],
-  variable: "--font-dm-sans",
-})
+  subsets: ['latin'],
+  weight: ['400', '500', '700', '1000'],
+  variable: '--font-dm-sans',
+});
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "Daniel Ekeberg",
-  description: "Daniel Ekeberg Portfolio",
+  title: 'Daniel Ekeberg',
+  description: 'Daniel Ekeberg Portfolio',
 };
 
 export default function RootLayout({
@@ -31,11 +32,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${dmSans.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <Analytics />
+      <body className={`${dmSans.variable} antialiased`}>{children}</body>
     </html>
   );
 }
